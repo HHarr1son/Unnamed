5f78d6bab7b6e3897dd3440a28b479fefbab0274
HHarr1son <hhsavethworld@gmail.com>
2025-08-14 18:52:49 +0800

    Add files via upload

---
+++ scripts/download_datasets.py
@@ -0,0 +1,9 @@
+# The dataset is recommended to be downloaded from the official website.
+
+# SEED: https://bcmi.sjtu.edu.cn/home/seed/
+# SEED-DV: https://bcmi.sjtu.edu.cn/home/seed/
+# DEAP: https://www.eecs.qmul.ac.uk/mmv/datasets/deap/
+# Things-EEG: https://things-initiative.org/
+# EYEEEGNET: http://www.eegeye.net
+
+
